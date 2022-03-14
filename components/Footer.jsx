65ae52e236e1035ccd915ab2bc2fd13dcfe9c451
David Diaz <daviddiazlds1221@gmail.com>
2022-03-14 18:13:36 -0500

    Se agrega año en el footer con el obj Date de js

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export const Footer = () => {
+
+  let date = new Date();
+
   return (
     <div>
         <footer className='bg-zinc-200 py-5 text-center'>
-            <p className='text-sky-500'><span className='text-black'>&copy; </span>Powered by David Diaz H.</p>
+            <p className='text-sky-500'><span className='text-black'>&copy; </span>Powered by David Diaz H - {date.getFullYear()}.</p>
             <p>This App is built with NextJS Full Stack · ReactJS · MySQL.</p>
         </footer>
     </div>
