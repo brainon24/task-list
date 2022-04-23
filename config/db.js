c8c67c5568389b6eecaf4fca76cb0b632132ae65
daviddiazh <daviddiazlds1221@gmail.com>
2022-04-23 17:46:02 -0500

    Se agregan nuevas propiedades a la DB con sql

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 import { createPool } from 'mysql2/promise';
 
-const pool = createPool({
-    host: 'bsfnc0gafyysw3twouo8-mysql.services.clever-cloud.com',
-    port: '3306',
-    user: 'ulrz3s1l8tajvlsx',
-    password: 'bcQmpc8x28RacGvBG8Y3',
-    database: 'bsfnc0gafyysw3twouo8',
-})
-
 // const pool = createPool({
-//     host: 'localhost',
+//     host: 'bsfnc0gafyysw3twouo8-mysql.services.clever-cloud.com',
 //     port: '3306',
-//     user: 'root',
-//     password: 'daviddiaz21',
-//     database: 'tasklist',
+//     user: 'ulrz3s1l8tajvlsx',
+//     password: 'bcQmpc8x28RacGvBG8Y3',
+//     database: 'bsfnc0gafyysw3twouo8',
 // })
+
+const pool = createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'daviddiaz21',
+    database: 'tasklist',
+})
 
 export { pool };
--- database/db.sql
+++ database/db.sql
@@ -5,9 +5,11 @@ USE tasklist;
 
 CREATE TABLE task (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description VARCHAR(400) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(450) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    responsible VARCHAR(20) NOT NULL,
     createdAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-describe product;
+describe task;
