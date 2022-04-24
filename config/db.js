8f1df3514db5877aebbbf30e0692ebb8db87ea87
daviddiazh <daviddiazlds1221@gmail.com>
2022-04-24 18:15:19 -0500

    connection MySQL remote

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
 import { createPool } from 'mysql2/promise';
 
-// const pool = createPool({
-//     host: 'bsfnc0gafyysw3twouo8-mysql.services.clever-cloud.com',
-//     port: '3306',
-//     user: 'ulrz3s1l8tajvlsx',
-//     password: 'bcQmpc8x28RacGvBG8Y3',
-//     database: 'bsfnc0gafyysw3twouo8',
-// })
-
 const pool = createPool({
-    host: 'localhost',
+    uri: 'mysql://uvoieb2qnwegknlr:mmedQi1Of3RTM1q63YQZ@bqkorgv1uheeffz3u40h-mysql.services.clever-cloud.com:3306/bqkorgv1uheeffz3u40h',
+    host: 'bqkorgv1uheeffz3u40h-mysql.services.clever-cloud.com',
     port: '3306',
-    user: 'root',
-    password: 'daviddiaz21',
-    database: 'tasklist',
-})
+    user: 'uvoieb2qnwegknlr',
+    password: 'mmedQi1Of3RTM1q63YQZ',
+    database: 'bqkorgv1uheeffz3u40h',
+});
+
+// const pool = createPool({
+//     host: 'localhost',
+//     port: '3306',
+//     user: 'root',
+//     password: 'daviddiaz21',
+//     database: 'tasklist',
+// })
 
 export { pool };
